cb16e91020e032b1bdb06950d42828f47512c3a8
honkly <honkly@163.com>
2019-07-25 23:33:54 +0800

    init

---
+++ game_stats.py
@@ -0,0 +1,18 @@
+class GameStats():
+    """跟踪游戏的统计信息"""
+    
+    def __init__(self, ai_settings):
+        """初始化统计信息"""
+        self.ai_settings = ai_settings
+        self.reset_stats()
+        
+        # 让游戏一开始处于非活动的状态
+        self.game_active = False
+        # 在任何情况下都不应重置最高得分
+        self.high_score = 0
+        
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
